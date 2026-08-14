44b549cd239c13c64a6b7bb5476b2d3cf534d2dd
unknown <leonk.poczta@protonmail.com>
2026-08-14 23:17:48 +0200

    Added animations, small change but wanted to continue building the habit of adding stuff, might get to adding enemies soon

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ base_dir = Path(__file__).resolve().parent
 image_dir = base_dir / "Images"
 
 
-pygame.display.set_icon(pygame.image.load(image_dir / "gameicon.png")) #REMEMBER TO ADD LINUX SUPPORT
+pygame.display.set_icon(pygame.image.load(image_dir / "gameicon.png"))
 pygame.display.set_caption("uhm")
 
 screen = pygame.display.set_mode((800, 600))
@@ -27,10 +27,25 @@ gravity = 0.5
 coyote_time = 0.1
 coyote_timer = 0.1
 
-playerleft = pygame.transform.scale(pygame.image.load(image_dir / "aple_left.png"), (42, 48))
-playerright = pygame.transform.scale(pygame.image.load(image_dir / "aple_right.png"), (42, 48))
+anim_timer = 0
+anim_speed = 0.1
+anim_frame = 0
 
-player = playerright
+playeridle = pygame.transform.scale(pygame.image.load(image_dir / "aple_idle.png"), (42, 48))
+playerfall = pygame.transform.scale(pygame.image.load(image_dir / "aple_fall.png"), (42, 48))
+
+
+player_right = [
+    pygame.transform.scale(pygame.image.load(image_dir / "aple_right1.png"), (42, 48)),
+    pygame.transform.scale(pygame.image.load(image_dir / "aple_right2.png"), (42, 48))
+]
+
+player_left = [
+    pygame.transform.scale(pygame.image.load(image_dir / "aple_left1.png"), (42, 48)),
+    pygame.transform.scale(pygame.image.load(image_dir / "aple_left2.png"), (42, 48))
+]
+
+player = playeridle
 
 player_rect = player.get_rect()
 player_rect.center = 400, 0 #spawn location
@@ -50,6 +65,20 @@ while running:
     dt = clock.tick(60) / 1000 
 
 #--inputs
+    if keys[pygame.K_a] or keys[pygame.K_d]:
+
+        anim_timer += dt
+
+        if anim_timer > anim_speed:
+            anim_timer = 0
+            anim_frame += 1
+
+            if anim_frame >= 2:
+                anim_frame = 0
+
+
+
+
     if keys[pygame.K_SPACE] and jumps_available > 0:
         if coyote_timer > 0 or on_ground or jumps_available < 2:
             if player_y_velocity >= 0:
@@ -58,12 +87,9 @@ while running:
                 jumps_available -= 1
 
     if keys[pygame.K_a]:
-        player = playerleft
         player_x_velocity -= acceleration
 
-
     if keys[pygame.K_d]:
-        player = playerright
         player_x_velocity += acceleration
 
 #--X movement
@@ -103,10 +129,12 @@ while running:
     old_bottom = player_rect.bottom
 
 #--move the player
+
     player_y_velocity += gravity
     player_rect.y += player_y_velocity
 
     on_ground = False
+
 
 #--collisions
     for platform in platforms:
@@ -122,6 +150,17 @@ while running:
         else:
             on_ground = False
             coyote_timer = max(0, coyote_timer - dt)
+
+#--animations
+    if player_y_velocity > 0 and not on_ground or jumps_available < 1:
+        player = playerfall
+    elif player_x_velocity > 0:
+        player = player_right[anim_frame]
+    elif player_x_velocity < 0:
+        player = player_left[anim_frame]
+    else: 
+        player = playeridle
+        
 
 #--stop the player from going out of bounds
     if player_rect.y > 600:
